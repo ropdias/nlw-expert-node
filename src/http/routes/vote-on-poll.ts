700ae588f18fd36ff97a48290a58963f7da624ff
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-03-05 22:10:54 -0300

    creating a cookie to vote on a poll

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { randomUUID } from 'node:crypto';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
 
@@ -15,6 +16,19 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPollBody.parse(request.body);
 
-    return reply.status(201).send();
+    let { sessionId } = request.cookies;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true, // protection to ensure they are not getting tampered with on client-side by an attacker
+        httpOnly: true, // protection to prevent attacks like XSS
+      });
+    }
+
+    return reply.status(201).send({ sessionId });
   });
 }
